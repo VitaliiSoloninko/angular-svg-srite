6427e2876d40b7fcad4a23e83718377be8d5388d
22051988Di <vitalii.soloninko@gmail.com>
2025-03-13 11:02:22 +0100

    main template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SvgBlockComponent } from './svg-block/svg-block.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SvgBlockComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-svg-srite';
---
+++ src/app/svg-block/svg-block.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SvgIconComponent } from '../ui/svg-icon/svg-icon.component';
+
+@Component({
+  selector: 'app-svg-block',
+  imports: [SvgIconComponent],
+  templateUrl: './svg-block.component.html',
+  styleUrl: './svg-block.component.scss',
+})
+export class SvgBlockComponent {}
